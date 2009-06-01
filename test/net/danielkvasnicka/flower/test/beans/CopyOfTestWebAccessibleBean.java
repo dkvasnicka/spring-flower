c63020f100cbea84d29931f23989a7dcac6e92fe
daniel.kvasnicka.jr <daniel.kvasnicka.jr@98cc8c02-4c35-11de-9ad6-1127a62b9fcd>
2009-06-01 12:30:51 +0000

    documentation for test beans
    git-svn-id: https://spring-flower.googlecode.com/svn/trunk@15 98cc8c02-4c35-11de-9ad6-1127a62b9fcd

--- test/net/danielkvasnicka/flower/test/beans/CopyOfTestWebAccessibleBean.java
+++ test/net/danielkvasnicka/flower/test/beans/CopyOfTestWebAccessibleBean.java
@@ -15,6 +15,14 @@ import net.danielkvasnicka.flower.response.api.Response;
  */
 public class CopyOfTestWebAccessibleBean extends AbstractWebAccessibleBean {
 
+	/**
+	 * This shows how to use JSP as a View. The map passed in
+	 * the second parameter contains values that are later set as request attributes 
+	 * before the forward to the JSP is executed.
+	 * 
+	 * @return
+	 */
+	@SuppressWarnings({ "unchecked", "serial" })
 	public Response hi() {
 		
 		return new ForwardingResponse("/WEB-INF/jsp/hi.jsp", new HashMap() {{ put("x", 6); }});
--- test/net/danielkvasnicka/flower/test/beans/TestWebAccessibleBean.java
+++ test/net/danielkvasnicka/flower/test/beans/TestWebAccessibleBean.java
@@ -13,12 +13,25 @@ import net.danielkvasnicka.flower.response.api.Response;
  *
  */
 public class TestWebAccessibleBean extends AbstractWebAccessibleBean {
-
+	
+	/**
+	 * Shows a primitive text streaming response, that prints 
+	 * value of one of the REST params
+	 * 
+	 * @return
+	 */
 	public Response index() {
 		
 		return new StreamingResponse("text/plain", "x = " + this.parameters.get("x"));
 	}
 
+	/**
+	 * And XML streaming response, that prints parameter "y", which is not encoded
+	 * in the URL as a REST param and should be sent through POST or GET.
+	 * This is to demo the functionality od merging REST and POST/GET params.
+	 * 
+	 * @return
+	 */
 	public Response hello() {
 		
 		return new StreamingResponse("text/xml", "<a f=\"" + this.parameters.get("y") + "\" />");
